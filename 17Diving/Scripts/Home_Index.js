3b5b82c6224e1a67a587a056ec09823bda6e1966
weihsun.1990 <weihsun.1990@gmail.com>
2017-11-10 01:39:35 +0800

    專案整理

--- 17Diving/Scripts/Home_Index.js
+++ 17Diving/Scripts/Home_Index.js
@@ -1,4 +1,15 @@
 ﻿
+
+$(document).ready(function () {
+
+    $('.item').on('click', function () {
+        var index = $(this).attr('data-item');
+        QQQ(index);
+    });
+
+});
+
+
 function QQQ(key) {
     var top;
     const singleHeight = document.body.clientHeight / 6;
--- 17Diving/obj/Release/Package/PackageTmp/Scripts/Home_Index.js
+++
@@ -1,6 +0,0 @@
-﻿
-function QQQ(key) {
-    var top;
-    const singleHeight = document.body.clientHeight / 6;
-    window.scrollTo(0, (singleHeight * (key - 1)));
-}
